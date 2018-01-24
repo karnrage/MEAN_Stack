44a07398ff8a4c250ba469f93adbf24aa50f93a8
Kamal <karnrage@sbcglobal.net>
2018-01-24 17:42:36 -0600

    current changes

--- fullstackIntegration/teamManager/teamManagerAngularApp/src/app/game-status/game-status.component.ts
+++ fullstackIntegration/teamManager/teamManagerAngularApp/src/app/game-status/game-status.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ManagerService } from '../manager.service';
+import { Player } from '../player';
+
 
 @Component({
   selector: 'app-game-status',
@@ -8,9 +10,19 @@ import { ManagerService } from '../manager.service';
 })
 export class GameStatusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _managerService: ManagerService) { }
+
+  playerList : any[]=[];
 
   ngOnInit() {
+    this._managerService.showPlayers();
+    this._managerService.playerObserver.subscribe(
+      (playerData)=>{
+        console.log("player data gathered or updated")
+        console.log(playerData); 
+        this.playerList = playerData; 
+      }
+    )
   }
 
 }
